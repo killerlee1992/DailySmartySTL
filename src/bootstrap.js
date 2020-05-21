1e3b42b0b400da8ebf06a347540fbe5d252fc02c
sjohnnlee <killerlee1992@gmail.com>
2020-05-21 10:20:53 -0700

    set app routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 
+import Home from './components/home'
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <switch>
+          <Route path='/' component={Home}/>
+          <Route path='/results' component={Home}/>
+        </switch>
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
