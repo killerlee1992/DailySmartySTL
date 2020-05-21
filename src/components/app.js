7c8633575feb6682825bb018b2a64a83105b8df2
sjohnnlee <killerlee1992@gmail.com>
2020-05-21 10:10:39 -0700

    set up redux form in searchbar.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
+import SearchBar from "./SearchBar";
+import RecentPosts from './recentPosts';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo/>
+        <SearchBar/>
+        <RecentPosts/>
       </div>
     );
   }
