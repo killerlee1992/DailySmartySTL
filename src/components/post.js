1a09a4c3b7ee76b98a27f255e4cf56def29eeb29
sjohnnlee <killerlee1992@gmail.com>
2020-05-29 11:46:52 -0700

    created a post component

---
+++ src/components/post.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Post extends Component {
+    
+  render() {
+    return <li>{this.props.title}</li>;
+  }
+}
+
+export default Post;
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
+import Post from './post';
+
 import * as actions from '../actions';
 
 class RecentPosts extends Component {
@@ -14,9 +16,7 @@ class RecentPosts extends Component {
         const posts = this.props.recentPosts.map((post, index) => {
             if(index < 3) {
                 return (
-                    <li key={index}>
-                        {post.title}
-                    </li>
+                   <Post {...post} key={index}/>
                 )   
             }
         })
