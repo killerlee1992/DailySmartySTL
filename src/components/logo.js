187f1346b848975df8d022be56601bbce11118e8
sjohnnlee <killerlee1992@gmail.com>
2020-06-03 12:35:48 -0700

    mapped state to props in results posts component and rendered each post with our post component, and implemented home link in logo component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Logo extends Component {
    render() {
@@ -8,7 +9,11 @@ export default class Logo extends Component {
        }
      return(
         <div className="logo-main">
-            <img style={size} src="/assets/ds_circle_logo.png"/>
+
+            <Link to="/">
+               <img style={size} src="/assets/ds_circle_logo.png"/> 
+            </Link>
+            
 
         </div>
             );
--- src/components/resultsPost.js
+++ src/components/resultsPost.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+import Post from "./post";
 
 class ResultsPosts extends Component {
+    renderPosts(){
+        const posts = this.props.posts.map((post,index) => {
+            return<Post key={index} {...post}/>
+        })
+        return posts;
+    }
   render() {
     return (
       <div className="results-post">
         <div className="results-posts-wrapper">
-          <ul className="results-posts">RESULTS GO HERE</ul>
+          <ul className="results-posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
@@ -15,7 +22,7 @@ class ResultsPosts extends Component {
 }
 function mapStateToProps(state) {
   return {
-    state,
+   posts: state.posts.resultsPosts
   };
 }
 export default connect(mapStateToProps)(ResultsPosts);
